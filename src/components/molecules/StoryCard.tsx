574aa5515a6686da65c719d4685eb3bf64d26a7e
ngbaduyy10 <ngbaduyy05@gmail.com>
2025-10-15 21:30:40 +0700

    add story card bg

--- src/components/molecules/StoryCard.tsx
+++ src/components/molecules/StoryCard.tsx
@@ -1,5 +1,7 @@
 import UserAvatar from '../atoms/UserAvatar';
 import { UserStory } from '@/dto/userStory.dto';
+import { StoryType } from '@/types';
+import Image from 'next/image';
 
 interface StoryCardProps {
   userStory: UserStory;
@@ -7,11 +9,33 @@ interface StoryCardProps {
 
 export default function StoryCard({ userStory }: StoryCardProps) {
   const { user, stories } = userStory;
+  const mainStory = stories[0];
+  
   return (
-    <div className="relative w-[130px] h-[180px] bg-primary rounded-xl overflow-hidden cursor-pointer flex-shrink-0">
+    <div 
+      className="relative w-[130px] h-[180px] rounded-xl overflow-hidden cursor-pointer flex-shrink-0"
+      style={mainStory.type === StoryType.TEXT && mainStory.background_color ? { backgroundColor: mainStory.background_color } : undefined}
+    >
+      {mainStory.type === StoryType.IMAGE && (
+        <Image
+          src={mainStory.url!}
+          alt={`${user.username}'s story`}
+          fill
+          className="object-cover"
+        />
+      )}
+      
+      {mainStory.type === StoryType.TEXT && (
+        <div className="absolute inset-0 flex items-center justify-center p-3">
+          <p className="text-white text-xs text-center line-clamp-6 break-words">
+            {mainStory.content}
+          </p>
+        </div>
+      )}
+      
       <UserAvatar 
         image={user.image}
-        className="absolute top-2 left-2 w-8 h-8 border-1 border-white"
+        className="absolute top-2 left-2 w-8 h-8 border-2 border-white z-10"
       />
     </div>
   )
